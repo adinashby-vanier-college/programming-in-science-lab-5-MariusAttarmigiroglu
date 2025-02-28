c439e6ba00c2611815edbb835b98d2c14ea2fa97
MariusAttarmigiroglu <mariusavedis@gmail.com>
2025-02-28 16:04:51 +0000

    Q3 done

--- Lab5.py
+++ Lab5.py
@@ -5,18 +5,23 @@
 # *   *
 # *****
 def hollow_square(n):
-    return ""
+   return ""
 
 # 1
 # 12
 # 123
 # 1234
 def number_pattern(n):
-    return ""
+   return ""
 
 # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
 def sum_of_natural_numbers(n):
-    return ""
+ total = 0
+ i = 1
+ while i <= n:
+  total += i
+  i += 1
+ return total
 
 # Example for n = 4:
 #    *
@@ -24,4 +29,9 @@ def sum_of_natural_numbers(n):
 #  *****
 # *******
 def centered_star_pyramid(n):
-    return ""
+  for i in range(0,n):
+   for j in range(0,n-i-1):
+    print(end= " ")
+   for j in range(0,i):
+    print ("*", end="")
+  return centered_star_pyramid
